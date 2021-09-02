39141fe174b7c8a5bfa01246623c24c03bed3a01
SamRei98 <samuli.reinikainen98@gmail.com>
2021-09-02 10:52:05 +0300

    Update nlp_helper_functions.py
    Added one_hot_encoding(), bag_of_words(), remove_emails() and remove_regex()

--- nlp_helper_functions.py
+++ nlp_helper_functions.py
@@ -2,12 +2,14 @@
     File name: nlp_helper_functions.py
     Author: Ali Salloum & Samuli Reinikainen
     Date created: 20/08/2021
-    Date last modified: 20/08/2021
+    Date last modified: 02/09/2021
     Python Version: 3.8
 '''
 
 import string
-
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.preprocessing import OneHotEncoder
+import re
 # LANGUAGE PREPROCESSING
 
 def address_nan(iterable):
@@ -20,13 +22,49 @@ def remove_punctuations(iterable):
     processed_iterable = [e.translate(str.maketrans('', '', string.punctuation)) for e in iterable]
     return processed_iterable
 
+def remove_emails(iterable):
+    " This function removes emails from the strings in iterable "
+    processed_iterable = [re.sub(r'\S+@\S+\.\S+', "", e) for e in iterabe]
+    assert len(processed_iterable) == len(iterable)
+    return processed_iterable
+
+def remove_regex(iterable, regex):
+    " This function removes any regex from the strings in the iterable "
+    processed_iterable = [re.sub(regex, "", e) for e in iterable]
+    assert len(processed_iterable) == len(iterable)
+    return processed_iterable
+    
 # VECTORIZATION
 
-def one_hot_encoding():
-    pass
+def one_hot_encoding(iterable, ignore_unknown = False, return_transformers = False):
+    " This function creates one hot encodings for a single feature or multiple features and returns the transformer if specified "
+    handle_unknown = "error"
+    if ignore_unknown:
+        handle_unknown = "ignore"
+    ohe = OneHotEncoder(handle_unknown = handle_unknown)
+    transformed = ohe.fit_transform(iterable)
+    assert len(transformed) == len(iterable)
+    if return_transformers:
+        return transformed, transformer
+    else:
+        return transformed
 
-def bag_of_words(tfidf=True):
-    pass
+def bag_of_words(iterable, max_features, ngram_range = (1,1), tfidf=True, return_transformers = False):
+    """ 
+        This function creates bag of words vectors for the iterable 
+        Returns the vector-iterable and the transformers if specified
+    """
+    vectorizer = CountVectorizer()
+    vectorized = vectorizer.fit_transform(iterable, max_features = max_features, ngram_range = ngram_range)
+    transformer = None
+    if tfidf:
+        transformer = TfidfTransformer()
+        vectorized = transformer.fit_transform(vectorized)
+    assert len(vectorized) == len(iterable)
+    if return_transformers:
+        return vectorized, (vectorizer, transformer)
+    else:
+        return vectorized
 
 def word_embedding():
     pass
